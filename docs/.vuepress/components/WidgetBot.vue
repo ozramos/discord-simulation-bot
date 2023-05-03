aa7cc1a92fc79d07753ec3e20094cfe3bb1d5c05
Oz Ramos <0xozram@gmail.com>
2023-05-03 13:24:35 -0700

    #3 embed widgetbot!

---
+++ docs/.vuepress/components/WidgetBot.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="widget"></div>
+</template>
+
+<script>
+export default {
+  name: 'WidgetBot',
+
+  mounted () {
+ // inject the following code into a ref
+ this.$refs.widget.innerHTML = `<widgetbot
+      server="1100098329255366767"
+      channel="1100298883931844648"
+      width="100%"
+      height="600"
+    ></widgetbot>`
+
+    const script = document.createElement('script')
+    script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/html-embed'
+    this.$refs.widget.appendChild(script)
+    console.log(this.$refs.widget)
+  }
+}
+</script>
