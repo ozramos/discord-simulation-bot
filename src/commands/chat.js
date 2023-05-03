76195d88256187747c875b306afdd5353a26c100
Oz Ramos <0xozram@gmail.com>
2023-05-03 13:24:35 -0700

    #2 reply to a bot with context

--- src/commands/chat.js
+++ src/commands/chat.js
@@ -21,8 +21,95 @@ client.on(Events.MessageCreate, async message => {
         message.channel.send(`<@${message.author.id}>`)
       }
     }
+
+    /**
+     * User replied to the bot, so we need to:
+     * 1. Get the user's memory
+     * 2. Add the user's message to their memory
+     * 3. Add the bot's response to their memory
+     * 4. Reply to the user with the bot's response
+     */
+    if (repliedMessage.author.id === client.user.id) {
+      // Get the user's memory
+      let messages = []
+      if (Memory.store[repliedMessage.author.name]) {
+        messages = Memory.store[repliedMessage.author.name]
+      }
+      
+      // Add the user's message to their memory
+      messages.push({role: 'user', content: message.content})
+      Memory.store[repliedMessage.author.name] = messages
+
+      // Generate a response
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages
+      })
+
+      // Add the bot's response to the user's memory
+      if (Memory.store[repliedMessage.author.name]) {
+        Memory.store[repliedMessage.author.name].push({role: 'assistant', content: completion.data.choices[0].message.content})
+      } else {
+        Memory.store[repliedMessage.author.name] = [
+          {role: 'assistant', content: completion.data.choices[0].message.content}
+        ]
+      }
+
+      // Reply to the user with the bot's response
+      await message.reply({content: completion.data.choices[0].message.content})
+    }
   }
 })
+
+
+
+
+/**
+ * Handle slash command
+ */
+async function execute (message) {
+  // Get the user's memory
+  let messages = []
+  if (Memory.store[message.user.id]) {
+    messages = Memory.store[message.user.id]
+  }
+  
+  await message.deferReply()
+  messages.push({role: 'user', content: message.options.getString('prompt', true)})
+  Memory.store[message.user.id] = messages
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages
+  })
+
+  try {
+    // Show an embed, with the users avatar and original prompt
+    // Uses the description field to display the bot's response
+    // Uses the footer field to show the time taken to generate the response
+    const embed = new EmbedBuilder()
+      .setAuthor({name: `${message.user.username}#${message.user.discriminator}`, iconURL: message.user.displayAvatarURL()})
+      .setDescription(message.options.getString('prompt', true))
+      
+    await message.editReply({embeds: [embed]})
+    await message.followUp({content: completion.data.choices[0].message.content})
+
+    // Add to memory
+    if (Memory.store[message.user.id]) {
+      Memory.store[message.user.id].push({role: 'user', content: message.options.getString('prompt', true)})
+      Memory.store[message.user.id].push({role: 'assistant', content: completion.data.choices[0].message.content})
+    } else {
+      Memory.store[message.user.id] = [
+        {role: 'user', content: message.options.getString('prompt', true)},
+        {role: 'assistant', content: completion.data.choices[0].message.content}
+      ]
+    }
+  } catch (e) {
+    console.error(e)
+    await message.editReply('Something went wrong!')
+  }
+}
+
 
 /**
  * Handle slash command
@@ -40,46 +127,5 @@ module.exports = {
     ),
   
   // Execution
-  async execute(interaction) {
-    // Get the user's memory
-    let messages = []
-    if (Memory.store[interaction.user.id]) {
-      messages = Memory.store[interaction.user.id]
-    }
-    
-    await interaction.deferReply()
-    messages.push({role: 'user', content: interaction.options.getString('prompt', true)})
-    Memory.store[interaction.user.id] = messages
-
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages
-    })
-
-    try {
-      // Show an embed, with the users avatar and original prompt
-      // Uses the description field to display the bot's response
-      // Uses the footer field to show the time taken to generate the response
-      const embed = new EmbedBuilder()
-        .setAuthor({name: `${interaction.user.username}#${interaction.user.discriminator}`, iconURL: interaction.user.displayAvatarURL()})
-        .setDescription(interaction.options.getString('prompt', true))
-        
-      await interaction.editReply({embeds: [embed]})
-      await interaction.followUp({content: completion.data.choices[0].message.content})
-
-      // Add to memory
-      if (Memory.store[interaction.user.id]) {
-        Memory.store[interaction.user.id].push({role: 'user', content: interaction.options.getString('prompt', true)})
-        Memory.store[interaction.user.id].push({role: 'assistant', content: completion.data.choices[0].message.content})
-      } else {
-        Memory.store[interaction.user.id] = [
-          {role: 'user', content: interaction.options.getString('prompt', true)},
-          {role: 'assistant', content: completion.data.choices[0].message.content}
-        ]
-      }
-    } catch (e) {
-      console.error(e)
-      await interaction.editReply('Something went wrong!')
-    }
-  }
+  execute
 }
